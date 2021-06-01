4f2fbf0679866b38705c1838b0825e0eb08b02a4
eiyiesanimo <37266042+eiyiesanimo@users.noreply.github.com>
2021-06-01 16:37:29 +0800

    Add files via upload

---
+++ Metrobankexam.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Metrobankexam.aspx.cs" Inherits="metrobankexam.Metrobankexam" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+       Search Product ID: <asp:TextBox ID="txtProductID" runat="server"></asp:TextBox><asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" />
+        <hr />
+        <table>
+            <tr>
+                <td>Product ID</td>
+                 <td>Product Name</td>
+                 <td>Price</td>
+                 <td>Qty</td>
+               
+            </tr>
+           <tr>
+                <td>
+                    <asp:Label ID="lblProductID" runat="server" Text=""></asp:Label></td>
+               
+               <td>
+                    <asp:Label ID="lblProductName" runat="server" Text=""></asp:Label></td>
+               <td>
+                    <asp:Label ID="lblPrice" runat="server" Text=""></asp:Label></td>
+               <td>
+                     <asp:TextBox ID="txtQty" runat="server"></asp:TextBox></td>
+                <td>
+                    <asp:Button ID="btnAddtoCart" runat="server" Text="Add to Cart" OnClick="btnAddtoCart_Click" /></td>
+           </tr>
+        </table>
+        <hr />
+        <asp:Label ID="lblOrderList" runat="server" Text="Ordered List"></asp:Label>
+        
+        <hr />
+        <asp:GridView ID="GridOrder" runat="server"></asp:GridView>
+
+        Total Amount: <asp:Label ID="txtTotalAmount" runat="server" Text=""></asp:Label><br />
+        Cash: <asp:TextBox ID="txtCash" runat="server"></asp:TextBox><br />
+        Change: <asp:Label ID="txtChange" runat="server" Text=""></asp:Label><br />
+        <asp:Button ID="SaveTransaction" runat="server" Text="Save Transaction" OnClick="SaveTransaction_Click" />
+        <hr />
+        <asp:Label ID="txtSuccess" runat="server" Text=""></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Metrobankexam.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace metrobankexam
+{
+    public partial class Metrobankexam : System.Web.UI.Page
+    {
+        string constring = ConfigurationManager.ConnectionStrings["constring"].ConnectionString;
+       
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(constring);
+            string searchsql = "Select * from Products where productID = " + txtProductID.Text;
+            SqlCommand cmd = new SqlCommand(searchsql, con);
+
+            con.Open();
+
+            SqlDataReader rd = cmd.ExecuteReader();
+           
+           if (rd.Read())
+            {
+                lblProductID.Text = rd.GetValue(0).ToString();
+                lblProductName.Text = rd.GetValue(1).ToString();
+                lblPrice.Text = rd.GetValue(2).ToString();
+            }
+           
+
+        }
+
+        protected void btnAddtoCart_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("Product Name");
+            dt.Columns.Add("Price");
+            dt.Columns.Add("Qty");
+            dt.Columns.Add("Amount");
+
+            int total = Int32.Parse(txtQty.Text) * Int32.Parse(lblPrice.Text);
+            txtTotalAmount.Text = total.ToString();
+
+            DataRow products = dt.NewRow();
+            products[0] = lblProductName.Text;
+            products[1] = lblPrice.Text;
+            products[2] = txtQty.Text;
+            products[3] = txtTotalAmount.Text;
+            dt.Rows.Add(products);
+            GridOrder.DataSource = dt;
+            GridOrder.DataBind();
+
+        }
+
+        protected void SaveTransaction_Click(object sender, EventArgs e)
+        {
+            int change = Int32.Parse(txtCash.Text) - Int32.Parse(txtTotalAmount.Text);
+            txtChange.Text = change.ToString();
+
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("Product Name");
+            dt.Columns.Add("Price");
+            dt.Columns.Add("Qty");
+            dt.Columns.Add("Amount");
+
+            int total = Int32.Parse(txtQty.Text) * Int32.Parse(lblPrice.Text);
+            txtTotalAmount.Text = total.ToString();
+
+            DataRow products = dt.NewRow();
+            products[0] = lblProductName.Text;
+            products[1] = lblPrice.Text;
+            products[2] = txtQty.Text;
+            products[3] = txtTotalAmount.Text;
+            dt.Rows.Add(products);
+            GridOrder.DataSource = dt;
+            GridOrder.DataBind();
+
+            txtSuccess.Text = "Transaction has been successfully saved!";
+        }
+    }
+}
